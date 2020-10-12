b622e9be87e6415edc3e426f0365dc348565668d
Steve Elliott <steve@elliottbrand.com>
2020-10-12 09:48:39 -0400

    Adjustments to get social icons displaying, hide the recaptcha badge, and display the legal notice in the form.

--- BlazorResume/Server/wwwroot/js/site.js
+++ BlazorResume/Server/wwwroot/js/site.js
@@ -1,19 +1,21 @@
 ﻿window.googleMethods = {
     loadGoogleAnalytics: function (pathAndQuery, GoogleAnalyticsID) {
-        var script = document.createElement("script");
-        script.async = true;
-        script.src = "https://www.googletagmanager.com/gtag/js?id=" + GoogleAnalyticsID;
-        script.id = "g-analytics-script";
+        if (GoogleAnalyticsID) {
+            var script = document.createElement("script");
+            script.async = true;
+            script.src = "https://www.googletagmanager.com/gtag/js?id=" + GoogleAnalyticsID;
+            script.id = "g-analytics-script";
 
-        if (!document.getElementById('g-analytics-script')) {
-            document.getElementsByTagName('head')[0].appendChild(script);
+            if (!document.getElementById('g-analytics-script')) {
+                document.getElementsByTagName('head')[0].appendChild(script);
+            }
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+
+            gtag('config', GoogleAnalyticsID, { page_path: pathAndQuery });
         }
-
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', GoogleAnalyticsID, { page_path: pathAndQuery });
     },
     loadCaptchaHeadScript: function (ReCaptchaPublicKey) {
         var script = document.createElement("script");
